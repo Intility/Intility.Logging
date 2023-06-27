2a7426048c5d0d7068bf133c37ab3cc99886b9a3
Thorstein Løkensgard <loekensgard+github@gmail.com>
2023-06-27 13:25:20 +0200

    feat: allow configuring elastic options (#100)
    * feat: add batchAction to elastic options
    
    * feat: add action delegate for additional elasticsearch sink options

--- src/Intility.Extensions.Logging.Elasticsearch/LoggerBuilderExtensions.cs
+++ src/Intility.Extensions.Logging.Elasticsearch/LoggerBuilderExtensions.cs
@@ -8,14 +8,27 @@ namespace Intility.Extensions.Logging
     public static class LoggerBuilderExtensions
     {
         /// <summary>
+        /// Registers Elasticsearch sink configuration if defined in the <paramref name="configure"/>
+        /// </summary>
+        /// <param name="configure">An action to configure the Elasticsearch sink options.</param>
+        /// <returns>The updated <see cref="ILoggerBuilder"/> instance.</returns>
+        public static ILoggerBuilder UseElasticsearch(this ILoggerBuilder builder, Action<ElasticsearchSinkOptions> configure)
+        {
+            //To prevent a StackOverflowException, hardcode the config section name instead of risking self-calling within the method.
+            builder.UseElasticsearch("Elasticsearch", configure);
+            return builder;
+        }
+
+        /// <summary>
         /// Uses Elasticsearch sink if Endpoints is defined in the <paramref name="configSection"/>.
         /// <br />
         /// IndexFormat is required in config. Optionally specify Username and Password for BasicAuth
         /// </summary>
         /// <param name="builder"></param>
         /// <param name="configSection"></param>
+        /// <param name="configure"></param>
         /// <returns></returns>
-        public static ILoggerBuilder UseElasticsearch(this ILoggerBuilder builder, string configSection = "Elasticsearch")
+        public static ILoggerBuilder UseElasticsearch(this ILoggerBuilder builder, string configSection = "Elasticsearch", Action<ElasticsearchSinkOptions> configure = null)
         {
             var elasticConfig = builder.Host.Configuration.GetSection(configSection);
             var elasticEndpoints = elasticConfig["Endpoints"];
@@ -30,7 +43,7 @@ namespace Intility.Extensions.Logging
             var indexFormat = elasticConfig["IndexFormat"];
             var versionString = elasticConfig["Version"];
 
-            if(string.IsNullOrWhiteSpace(indexFormat))
+            if (string.IsNullOrWhiteSpace(indexFormat))
             {
                 throw new Exception("Failed to initialize Elasticsearch sink", 
                     new ArgumentException($"missing elastic config: {configSection}:IndexFormat"));
@@ -58,8 +71,9 @@ namespace Intility.Extensions.Logging
                 options.ModifyConnectionSettings = c => c.BasicAuthentication(username, password);
             }
 
-            builder.Configuration.WriteTo.Elasticsearch(options);
+            configure?.Invoke(options);
 
+            builder.Configuration.WriteTo.Elasticsearch(options);
             return builder;
         }
     }
