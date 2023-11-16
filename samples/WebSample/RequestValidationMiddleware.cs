d355ef07702be0d2edcd96939c13a35a03bae9ce
Thorstein Løkensgard <thorstein.lokensgard@intility.no>
2023-11-16 14:44:15 +0100

    feat: add update versjon to .net 8

--- samples/WebSample/RequestValidationMiddleware.cs
+++ samples/WebSample/RequestValidationMiddleware.cs
@@ -1,6 +1,18 @@
-﻿namespace WebSample
+﻿using Microsoft.AspNetCore.Http;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace WebSample
 {
-    internal class RequestValidationMiddleware
+    internal class RequestValidationMiddleware : IMiddleware
     {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var path = context.Request.Path.Value.ToLower();
+
+            Log.Information("Path requested: {path}", path);
+
+            await next(context);
+        }
     }
 }
--- samples/WebSample/Startup.cs
+++ samples/WebSample/Startup.cs
@@ -19,6 +19,7 @@ namespace WebSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<RequestValidationMiddleware>();
             services.AddControllersWithViews();
         }
 
